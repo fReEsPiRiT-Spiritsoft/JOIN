a040faccb44dd3a2a8043470c1a4cdc5f3986752
Daniel <Danielluzius@mail.de>
2025-11-07 11:40:11 +0100

    fix: import bug with legal notice and privacy policy

--- src/app/features/legal-notice/legal-notice.ts
+++ src/app/features/legal-notice/legal-notice.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
